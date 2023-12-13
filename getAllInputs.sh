a271dffef284d8d69cff50bc4ed1be5ee2fd2787
Celia Maginot <celia.maginot@orange.fr>
2023-12-13 16:13:07 +0100

    fix some issue on getInputs

--- getAllInputs.sh
+++ getAllInputs.sh
@@ -2,5 +2,13 @@ echo "downloading all Input ..."
 
 for dir in $(ls -d */); do
     echo "$(tput setaf 6)=====GETTING INPUT - $(echo $dir | cut -d'/' -f1)=====$(tput setaf 0)"
+    if [ ! -d "$dir/input" ]; then
+        mkdir $dir/input
+        echo "creating input folder"
+    fi
+    if [ ! -d "$dir/example" ]; then
+        mkdir $dir/example
+        echo "creating example folder"
+    fi
     make -C $dir getInput
 done
